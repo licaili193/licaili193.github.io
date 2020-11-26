bd3fbec16de3323f540ef75634dfb5148c06a92c
licaili193 <caili@udel.edu>
2020-11-25 16:54:15 -0800

    update

---
+++ typing.js
@@ -0,0 +1,63 @@
+var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period) || 4000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+    var quarterRemainPeriod = this.period / 8;
+    var quarterTypePeriod = this.period / 8 / (fullTxt.length - 1);
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">' + this.txt+'</span>';
+  
+    var that = this;
+    var delta = 0;
+  
+    if (this.isDeleting) {
+        delta = quarterTypePeriod; 
+    } else {
+        delta = quarterTypePeriod;
+    }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = 5 * quarterRemainPeriod;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = quarterRemainPeriod;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #F26921 }";
+    document.body.appendChild(css);
+  };
+  
