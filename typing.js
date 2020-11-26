4f6211c73496e23a8fa9a043ba6689e20d84c621
licaili193 <caili@udel.edu>
2020-11-25 17:51:44 -0800

    update

--- typing.js
+++ typing.js
@@ -1,15 +1,29 @@
-var TxtRotate = function(el, toRotate, period) {
+var TxtRotate = function(el, toRotate, period, sl, toSlide) {
     this.toRotate = toRotate;
+    this.toSlide = toSlide;
     this.el = el;
+    this.sl = sl;
     this.loopNum = 0;
     this.period = parseInt(period) || 4000;
     this.txt = '';
     this.tick();
     this.isDeleting = false;
+    this.toBegin = true;
   };
   
   TxtRotate.prototype.tick = function() {
     var i = this.loopNum % this.toRotate.length;
+    if (this.toBegin) {
+        var span = this.sl.getElementsByTagName('span')[0];
+        span.style.backgroundImage = "url('" + this.toSlide[i] + "')";
+        span.classList.remove("slideshow-span-animation");
+        window.requestAnimationFrame(function(time) {
+            window.requestAnimationFrame(function(time) {
+                span.classList.add("slideshow-span-animation");
+            });
+        });
+        this.toBegin = false;
+    }
     var fullTxt = this.toRotate[i];
     var quarterRemainPeriod = 3 * this.period / 16;
     var halfTypePeriod = this.period / 8 / fullTxt.length;
@@ -32,6 +46,7 @@ var TxtRotate = function(el, toRotate, period) {
       this.isDeleting = false;
       this.loopNum++;
       delta = quarterRemainPeriod;
+      this.toBegin = true;
     }
   
     setTimeout(function() {
@@ -41,11 +56,13 @@ var TxtRotate = function(el, toRotate, period) {
   
   window.onload = function() {
     var elements = document.getElementsByClassName('txt-rotate');
+    var slides = document.getElementsByClassName('slideshow');
     for (var i=0; i<elements.length; i++) {
       var toRotate = elements[i].getAttribute('data-rotate');
       var period = elements[i].getAttribute('data-period');
+      var toSlide = slides[i].getAttribute('data-rotate');
       if (toRotate) {
-        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        new TxtRotate(elements[i], JSON.parse(toRotate), period, slides[i], JSON.parse(toSlide));
       }
     }
     // INJECT CSS
