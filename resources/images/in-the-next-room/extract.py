7ec20b8f845b4da56e0a304b30ea48ca0bb5c4e5
licaili193 <caili@udel.edu>
2020-12-15 00:31:45 -0800

    update

--- resources/images/in-the-next-room/extract.py
+++ resources/images/in-the-next-room/extract.py
@@ -4,7 +4,7 @@ import pyperclip
 path = os.path.dirname(os.path.realpath(__file__))
 files = os.listdir(path)
 for f in files[:]:
-    if not(f.endswith((".png", ".jpg", ".jpeg"))):
+    if not(f.endswith((".png", ".jpg", ".jpeg", ".JPG"))):
         files.remove(f)
 
 pyperclip.copy(str(files).replace(",", ",\n"))
